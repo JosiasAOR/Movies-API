4d9c9f16be89c0fec57317b2ade71b25864283d0
JosiasAOR <josiasaor@gmail.com>
2023-09-25 19:38:19 -0300

    chore/main: install cors in project

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const moviesRoutes = require("./routes/movies");
 const app = express();
+const cors = require('cors'); // Importe o pacote cors
 
+app.use(cors());
 app.use("/", moviesRoutes);
 
 app.listen(3000, () => {
